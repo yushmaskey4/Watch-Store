1857c7a4be82d39a513d43af75cfeaaced6e91eb
Yush Maskey <yushmaskey4@gmail.com>
2026-02-25 02:22:22 +0545

    fixed category links to /products

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,9 @@ import CategoryCard from "../components/CategoryCard";
 
 const Home = () => {
   const categories = [
-    { id: 1, name: "Luxury", image: "https://images.unsplash.com/photo-1523170335258-f5ed11844a49?w=600", link: "/products/Luxury" },
-    { id: 2, name: "Sports", image: "https://images.unsplash.com/photo-1691439378545-dd6b35ff2f7b?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8c3BvcnRzJTIwd2F0Y2h8ZW58MHx8MHx8fDA%3D", link: "/products/Sports" },
-    { id: 3, name: "Smart", image: "https://images.unsplash.com/photo-1579586337278-3befd40fd17a?q=80&w=1172&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", link: "/products/Smart" },
+    { id: 1, name: "Luxury", image: "https://images.unsplash.com/photo-1523170335258-f5ed11844a49?w=600", link: "/products" },
+    { id: 2, name: "Sports", image: "https://images.unsplash.com/photo-1691439378545-dd6b35ff2f7b?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8c3BvcnRzJTIwd2F0Y2h8ZW58MHx8MHx8fDA%3D", link: "/products" },
+    { id: 3, name: "Smart", image: "https://images.unsplash.com/photo-1579586337278-3befd40fd17a?q=80&w=1172&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", link: "/products" },
   ];
 
   return (
